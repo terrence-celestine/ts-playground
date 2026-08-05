b72409f74a2cfe9410d0f3f17a720cf65cf70fe1
Terrence Celestine <terrencebutler89@gmail.com>
2026-08-04 17:36:59 -0700

    fix: added isAdmin guard function

--- src/generics/type-guard/isAdmin.ts
+++ src/generics/type-guard/isAdmin.ts
@@ -1,5 +1,5 @@
 import { User, Admin } from "../../shared/user"
 
-export const isAdmin = (x: User | Admin): x is User => {
-    return (x as Admin).role === "admin"
+export const isAdmin = (x: User | Admin): x is Admin => {
+    return x.role === "admin"
 }
